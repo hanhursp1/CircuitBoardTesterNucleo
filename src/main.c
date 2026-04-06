4f21e64b7fa3691dac1521c4680e0744d09a6bc2
hanhursp1 <hanhursp1@newpaltz.edu>
2026-04-06 14:12:51 -0400

    Updated main gcode testing fn to support all three steppers

--- src/main.c
+++ src/main.c
@@ -12,129 +12,160 @@
 #include <stdio.h>
 #include <time.h>
 
-#define LED_PIN                                GPIO_PIN_5
-#define LED_GPIO_PORT                          GPIOA
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED_PIN GPIO_PIN_5
+#define LED_GPIO_PORT GPIOA
+#define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOA_CLK_ENABLE()
 
 void LED_Init();
 
-Stepper test_stepper;
+Stepper stepper_Z;
+Stepper stepper_L;
+Stepper stepper_R;
 
-void __gcode_assert(const char * filename, int line, const char * funcname, const char * what_broke) {
-	printf("Exception in file: \"%s:%d\": \n\tFunction \"%s\": `%s` evaluated to false", filename, line, funcname, what_broke);
+void __gcode_assert(const char *filename, int line, const char *funcname,
+                    const char *what_broke) {
+  printf("Exception in file: \"%s:%d\": \n\tFunction \"%s\": `%s` evaluated to "
+         "false",
+         filename, line, funcname, what_broke);
 }
 
-void __assert_func(const char * filename, int line, const char * funcname, const char * what_broke) {
-	printf("Exception in file: \"%s:%d\": \n\tFunction \"%s\": `%s` evaluated to false", filename, line, funcname, what_broke);
-	while (true) {} // Loop forever
+void __assert_func(const char *filename, int line, const char *funcname,
+                   const char *what_broke) {
+  printf("Exception in file: \"%s:%d\": \n\tFunction \"%s\": `%s` evaluated to "
+         "false",
+         filename, line, funcname, what_broke);
+  while (true) {
+  } // Loop forever
 }
 
 // Placeholder gcode execution function
 void exec_gcode(Gcode gcode) {
-	gcode_assert(gcode.num_args >= 1);
-	// Switch based on command group (this is the letter of the command)
-	switch (gcode.args[0].id) {
-		case 'R': {
-			// Switch based on command id (this is the number of the command)
-			switch (gcode.args[0].value) {
-				/*  Command R20:
-				 *    Commands R20 R{N} and R20 L{N} rotate the stepper motor by N steps clockwise
-				 *    and counterclockwise respectfully.
-				 */
-				case 20: {
-					// Assert that the input is valid. Panic if not.
-					gcode_assert(gcode.num_args >= 2);
-					char id = gcode.args[1].id;
-					gcode_assert(id == 'L' || id == 'R');
+  gcode_assert(gcode.num_args >= 1);
+  // Switch based on command group (this is the letter of the command)
+  switch (gcode.args[0].id) {
+  case 'R': {
+    Stepper *s;
+    char *stepper_name;
+    // Switch based on command id (this is the number of the command)
+    switch (gcode.args[0].value) {
+    /*  Command R20:
+     *    Commands R20 R{N} and R20 L{N} rotate the stepper motor by N steps
+     * clockwise and counterclockwise respectfully.
+     */
+    // goto considered harmful, but I like to live dangerously
+    case 20:
+      s = &stepper_Z;
+      stepper_name = "Stepper Z";
+      break;
+    case 30:
+      s = &stepper_L;
+      stepper_name = "Stepper L";
+      break;
+    case 40:
+      s = &stepper_R;
+      stepper_name = "Stepper R";
+      break;
+    default:
+      printf("Unrecognized gcode instruction: %c%d", gcode.args[0].id,
+             gcode.args[0].value);
+      break;
+    }
+    // Assert that the input is valid. Panic if not.
+    gcode_assert(gcode.num_args >= 2);
+    char id = gcode.args[1].id;
+    gcode_assert(id == 'L' || id == 'R');
 
-					// Get the direction based on the argument id, and the number of turns
-					// based on the argument value
-					StepperDirection dir = (id == 'L') ? STEPD_COUNTERCLOCKWISE : STEPD_CLOCKWISE;
-					int num_turns = gcode.args[1].value;
+    // Get the direction based on the argument id, and the number of turns
+    // based on the argument value
+    StepperDirection dir =
+        (id == 'L') ? STEPD_COUNTERCLOCKWISE : STEPD_CLOCKWISE;
+    int num_turns = gcode.args[1].value;
 
-					// Alert the user over USART stdout
-					printf("Turning %s %d ticks...\n", (dir == STEPD_CLOCKWISE) ? "clockwise" : "counterclockwise",  num_turns);
-					fflush(stdout);
-					// Set the direction of the stepper
-					Stepper_set_direction(&test_stepper, dir);
-					// Actually rotate the stepper
-					for (int i = 0; i < num_turns; i++) {
-						Stepper_step_immediate(&test_stepper);
-					}
-				} break;
-				default: {
-					printf("Unrecognized gcode instruction: %c%d", gcode.args[0].id, gcode.args[0].value);
-				} break;
-			}
-		} break;
-		case 'G': {
-			switch (gcode.args[0].value) {
-				/*  Command G00:
-				 *    Simple debug command that toggles the on-board LED
-				 */
-				case 0: {
-					HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-				} break;
-			}
-		} break;
-		default: {
-			printf("Unrecognized gcode instruction: %c%d", gcode.args[0].id, gcode.args[0].value);
-		} break;
-	}
+    // Alert the user over USART stdout
+    printf("Turning %s %s %d ticks...\n", stepper_name,
+           (dir == STEPD_CLOCKWISE) ? "clockwise" : "counterclockwise",
+           num_turns);
+    fflush(stdout);
+    // Set the direction of the stepper
+    Stepper_set_direction(s, dir);
+    // Actually rotate the stepper
+    for (int i = 0; i < num_turns; i++) {
+      Stepper_step_immediate(s);
+    }
+  } break;
+  case 'G': {
+    switch (gcode.args[0].value) {
+    /*  Command G00:
+     *    Simple debug command that toggles the on-board LED
+     */
+    case 0: {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    } break;
+    }
+  } break;
+  default: {
+    printf("Unrecognized gcode instruction: %c%d", gcode.args[0].id,
+           gcode.args[0].value);
+  } break;
+  }
 }
 
 int main(void) {
-	// Init Hardware Abstraction Layer
+  // Init Hardware Abstraction Layer
   HAL_Init();
-	// Enable GPIOB
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	// Init LED for lights and stuff :)
+  // Enable GPIOB
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  // Init LED for lights and stuff :)
   LED_Init();
 
-	// Do a delay before working any further (fixes something, I forget what.)
-	HAL_Delay(1000);
+  // Do a delay before working any further (fixes something, I forget what.)
+  HAL_Delay(1000);
 
-	// Initialize USB serial, which also initializes the stdout and stdin
-	USB_init();
+  // Initialize USB serial, which also initializes the stdout and stdin
+  USB_init();
 
-	test_stepper.io = (StepperIO){
-		.gpio = GPIOB, .step = GPIO_PIN_13, .dir = GPIO_PIN_14
-	};
-	Stepper_init_simplified(&test_stepper);
+  stepper_Z.io =
+      (StepperIO){.gpio = GPIOB, .step = GPIO_PIN_5, .dir = GPIO_PIN_6};
+  stepper_L.io =
+      (StepperIO){.gpio = GPIOB, .step = GPIO_PIN_1, .dir = GPIO_PIN_2};
+  stepper_R.io =
+      (StepperIO){.gpio = GPIOB, .step = GPIO_PIN_3, .dir = GPIO_PIN_4};
+  Stepper_init_simplified(&stepper_Z);
+  Stepper_init_simplified(&stepper_L);
+  Stepper_init_simplified(&stepper_R);
 
-	HAL_Delay(100);
+  HAL_Delay(100);
 
-	Stepper_set_direction(&test_stepper, STEPD_CLOCKWISE);
+  Stepper_set_direction(&stepper_Z, STEPD_CLOCKWISE);
 
-	// Prompt the user for a gcode instruction, and then execute it.
-	while (true) {
-		printf("Enter an instruction: ");
+  // Prompt the user for a gcode instruction, and then execute it.
+  while (true) {
+    printf("Enter an instruction: ");
 
-		// Decode the next valid instruction and then flush the USART
-		Gcode test = gcode_decode(stdin);
-		USART_flush(USB_USART);
+    // Decode the next valid instruction and then flush the USART
+    Gcode test = gcode_decode(stdin);
+    USART_flush(USB_USART);
 
-		// Print the commmand we got
-		printf("Got %d args: ", test.num_args);
-		for (int i = 0; i < test.num_args; i++) {
-			printf("%c%d ", test.args[i].id, test.args[i].value);
-		}
-		printf("\n");
-		// Run the command
-		exec_gcode(test);
-	}
+    // Print the commmand we got
+    printf("Got %d args: ", test.num_args);
+    for (int i = 0; i < test.num_args; i++) {
+      printf("%c%d ", test.args[i].id, test.args[i].value);
+    }
+    printf("\n");
+    // Run the command
+    exec_gcode(test);
+  }
 
-  while (1)
-  {
-		// Print to stdout and flush it
-		// printf("Hello world! %d\n", i++);
-		fflush(stdout);
-		
-		// Toggle onboard LED
-		GPIOA->ODR |= 0x0020;
+  while (1) {
+    // Print to stdout and flush it
+    // printf("Hello world! %d\n", i++);
+    fflush(stdout);
+
+    // Toggle onboard LED
+    GPIOA->ODR |= 0x0020;
     HAL_Delay(1000);
-		GPIOA->ODR &= ~0x0020;
-		HAL_Delay(1000);
+    GPIOA->ODR &= ~0x0020;
+    HAL_Delay(1000);
   }
 }
 
@@ -148,6 +179,4 @@ void LED_Init() {
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 }
 
-void SysTick_Handler(void) {
-  HAL_IncTick();
-}
+void SysTick_Handler(void) { HAL_IncTick(); }
